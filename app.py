576360568622214fa653b193c8c340fcc5869dca
UMAR0800 <148867003+UMAR0800@users.noreply.github.com>
2024-10-01 14:12:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Jenkins!"
+    return "Hello, To the world!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)  # Change the port to 5000
